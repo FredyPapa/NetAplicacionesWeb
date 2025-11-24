3d5abee60420edd64f870909016e9bdcd78af905
Fredy <fredy.luis.wolf@gmail.com>
2025-11-24 17:16:27 -0500

    Clase 06 - Parcial 2

--- src/ECommerceWeb.WebApp/Program.cs
+++ src/ECommerceWeb.WebApp/Program.cs
@@ -2,13 +2,21 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ECommerceWeb.WebApp;
 using CurrieTechnologies.Razor.SweetAlert2;
+using Blazored.Toast;
+using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using ECommerceWeb.WebApp.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddBlazoredToast();
+builder.Services.AddBlazoredSessionStorage();
 builder.Services.AddSweetAlert2();
+
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationService>();
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
